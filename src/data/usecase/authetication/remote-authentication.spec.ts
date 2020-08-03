ae6aa865ad126aaf0d92a5110a66798bedc3bec1
Janio Miara <janiomiara@hotmail.com>
2020-08-03 12:20:11 -0300

    feat: ensyre RemoteAuthenticatiom calls HttpPostClinet with corrent URL

---
+++ src/data/usecase/authetication/remote-authentication.spec.ts
@@ -0,0 +1,19 @@
+import { RemoteAuthentication } from './remote-authentication'
+import { HttpPostClient } from '../../protocols/http/http-post-clinet'
+
+describe('RemoteAuthentication', () => {
+  test('Should call HttpClient with correct URL', async () => {
+    class HttpPostClientSpy implements HttpPostClient {
+      url?: string
+      async post (url: string): Promise<void> {
+        this.url = url
+        return Promise.resolve()
+      }
+    }
+    const url = 'any_url'
+    const httpPostClient = new HttpPostClientSpy()
+    const sut = new RemoteAuthentication(url, httpPostClient)
+    await sut.auth()
+    expect(httpPostClient.url).toBe(url)
+  })
+})
