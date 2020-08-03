b1b5c1eb7237dda9f14daaf1bdb11c8a87c4ecdd
Janio Miara <janiomiara@hotmail.com>
2020-08-03 14:48:16 -0300

    chore: ensure add faker

--- src/data/protocols/http/http-post-clinet.ts
+++ src/data/protocols/http/http-post-clinet.ts
@@ -1,3 +1,8 @@
-export interface HttpPostClient {
-  post(url: string): Promise<void>
+export type HttpPostParams = {
+  url: string
+  body?: object
+}
+
+export interface HttpPostClient{
+  post(params: HttpPostParams): Promise<void>
 }
